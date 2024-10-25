b1f13521f2d6bd0d4e088fe0a63f7430f8c1ccc1
Heshbon <cityalight.hesbon@gmail.com>
2024-10-25 18:10:32 +0300

    Create User model with comprehensive user schema.

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,26 +2,26 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username: {
-        type: String,
-        required: true,
+        type:String,
+        required:true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+        type:String,
+        required:true,
+        unique:true,
     },
     password: {
-        type: String,
-        required: true,
+        type:String,
+        required:true,
     },
-    firstName: String,
-    lastName: String,
-    bio: String,
-    gender: String,
-    dateOfBirth: Date,
-    location: String,
+    firstName:String,
+    lastName:String,
+    bio:String,
+    gender:String,
+    dateOfBirth:Date,
+    location:String,
 }, {
-    timestamps: true,
+    timestamps:true,
 });
 
 module.exports = mongoose.model("User", userSchema);

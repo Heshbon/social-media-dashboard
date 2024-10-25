0e034f07ad63dafa80faa501e4996dc543ec6cd4
Heshbon <cityalight.hesbon@gmail.com>
2024-10-25 18:06:28 +0300

    Add Comment model with schema definition for comments

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,10 +1,10 @@
 let mongoose = require("mongoose");
 
 let commentSchema = mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-    content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true},
+    content: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now}
 }, {
     collection: 'comments'
 });

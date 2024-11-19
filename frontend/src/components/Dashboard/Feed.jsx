fb28d1f51a7491eb378311d174a331c71cd38ac8
Heshbon <cityalight.hesbon@gmail.com>
2024-11-19 18:09:48 +0300

    fix feed post file

--- frontend/src/components/Dashboard/Feed.jsx
+++ frontend/src/components/Dashboard/Feed.jsx
@@ -3,13 +3,13 @@ import {usePosts} from "../../context/PostContext";
 import Post from "./Post";
 
 const Feed = () => {
-  const {posts, loading, error, fetchPosts} = usePosts();
+  const {posts =[], loading, error, fetchPosts} = usePosts() || {};
 
   useEffect(() => {
-    if (!posts.length) {
+    if (!loading && !posts.length) {
       fetchPosts();
     }
-  }, [posts, fetchPosts]);
+  }, [loading, posts.length, fetchPosts]);
     
   if (loading) return <p>Loading...</p>;
   if (error) return <p style={{color:"red"}}>{error}</p>;
